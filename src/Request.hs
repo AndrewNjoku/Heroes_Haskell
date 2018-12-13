0871adc6bb7e1f331ff0df4c1775185ede23af33
Andria Njoku <andrianjoku@gmail.com>
2018-12-13 13:05:08 +0000

    For the Request we have added the getHero function which contains the logic for calling the more Abstract function getjSON

--- src/Request.hs
+++ src/Request.hs
@@ -4,6 +4,8 @@ module Request
 
 import JSON
 
+
+import Data.Aeson
 import GHC.Int
 import qualified Data.ByteString.Lazy.Internal as L
 import Network.HTTP.Conduit
@@ -14,6 +16,31 @@ import qualified Data.ByteString as D
 
 getJSON :: URL -> IO D.ByteString
 getJSON x = simpleHttp x
+
+
+getHeroes :: IO ()
+
+getHeroes = do
+
+ putStrLn " Retrieving Hero info...."
+
+ d <- (eitherDecode <$> getJSON heroURL) :: IO (Either String [Hero])
+
+ {- | If d is returned to the Left, the JSON was malformed.
+ -- In that case, we report the error.
+ -- Otherwise, we perform the operation of
+ -- our choice. In this case, just print it.
+ -}
+
+   case d of
+        Left err -> putStrLn err
+
+        {- | In getHeroes right signifies our IO action has been successfull and we want to pass 
+             our array of heroes to a Database function to further process. We do this using mapm_ as we 
+             want to perform an action and are not worried about returning any data but performing the side 
+             effect of adding these heroes to our database -}
+
+        Right ps -> mapm_ insertHeroes ps
 
 
 
